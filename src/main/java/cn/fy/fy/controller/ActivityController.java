282579b95d250d19e75fa28b80578b816c573a11
wwz <wwz18739290895@163.com>
2020-03-25 21:06:41 +0800

    1

--- src/main/java/cn/fy/fy/controller/ActivityController.java
+++ src/main/java/cn/fy/fy/controller/ActivityController.java
@@ -1,10 +1,14 @@
 package cn.fy.fy.controller;
 
 
+import cn.fy.fy.entity.Activity;
+import cn.fy.fy.service.IActivityService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,42 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping("/activity")
 public class ActivityController {
+    @Resource
+    private IActivityService iActivityService;
+
+    @Resource
+    private IActivityService  apl1;
+    @RequestMapping("/huo")
+    public String huo(){
+        return "huo";
+    }
+
+    @RequestMapping("/huo.html")
+    public String huo(Activity activity){
+        boolean i=iActivityService.save(activity);
+        if(i){
+            return "redirect:/";
+        }else {
+            return "huo";
+        }
+    }
+
+    @RequestMapping("/delhuo")
+    public String delhuo( Model model){
+        List<Activity> listActivity = apl1.list();
+        model.addAttribute("listActivity",listActivity);
+        return "delhuo";
+    }
+
+    @RequestMapping("/delhuo.html")
+    public String delete(Integer activityId){
+        int i=iActivityService.findActivity(activityId);
+        if(i>0){
+            return "redirect:/";
+        }else {
+            return "delhuo";
+        }
+    }
+
+
 }
